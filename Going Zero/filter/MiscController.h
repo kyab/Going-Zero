40d051500b248e246244e27412fcbb3d2fa11e04
KojiYoshioka <kyossi212@gmail.com>
2024-01-17 13:20:47 +0900

    Adding misc.

---
+++ Going Zero/filter/MiscController.h
@@ -0,0 +1,17 @@
+//
+//  MiscController.h
+//  Going Zero
+//
+//  Created by yoshioka on 2024/01/17.
+//  Copyright © 2024 kyab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MiscController : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Going Zero/filter/MiscController.m
@@ -0,0 +1,25 @@
+//
+//  MiscController.m
+//  Going Zero
+//
+//  Created by yoshioka on 2024/01/17.
+//  Copyright © 2024 kyab. All rights reserved.
+//
+
+#import "MiscController.h"
+#import "Bender.h"
+#import "TrillReverse.h"
+#import "Freezer.h"
+
+@interface MiscController ()
+
+@end
+
+@implementation MiscController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
